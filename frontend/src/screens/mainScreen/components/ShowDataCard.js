961e132b91b22d6e3ce89eed093febcc67cf5e25
MrMelada <74542758+LiroyMelamed@users.noreply.github.com>
2025-12-30 01:22:21 +0200

    Phase E: remove row-reverse RTL hack (ShowDataCard)

--- frontend/src/screens/mainScreen/components/ShowDataCard.js
+++ frontend/src/screens/mainScreen/components/ShowDataCard.js
@@ -14,19 +14,20 @@ export default function ShowDataCard({ title, icon, numberText, comprationNumber
             <TextBold36 style={{ marginTop: 8 }}>{numberText}</TextBold36>
 
             {comprationNumber && comprationText &&
-                <SimpleContainer style={{ display: 'flex', flexDirection: 'row-reverse', alignItems: 'center', marginTop: 8 }}>
+                <SimpleContainer style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: 8 }}>
+                    <Text12>{comprationText}</Text12>
+                    <Text12 color={comprationNumber > 0 ? colors.positive : colors.negative} style={{ marginInlineStart: 8 }}>{addCommasToNumber(comprationNumber, null, "%")}</Text12>
                     <SimpleIcon
                         src={icons.Button.DownArrow}
                         style={{
                             display: 'flex',
                             transform: `rotate(${comprationNumber > 0 ? 180 : 0}deg)`,
                             height: 12,
-                            width: 12
+                            width: 12,
+                            marginInlineStart: 8
                         }}
                         tintColor={comprationNumber > 0 ? colors.positive : colors.negative}
                     />
-                    <Text12 color={comprationNumber > 0 ? colors.positive : colors.negative} style={{ marginRight: 8 }}>{addCommasToNumber(comprationNumber, null, "%")}</Text12>
-                    <Text12 style={{ marginRight: 4 }}>{comprationText}</Text12>
                 </SimpleContainer>
             }
         </SimpleCard>
